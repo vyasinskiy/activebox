6052b9229f7c3127a66d818a4d7d7717405cc940
yasinsky93 <yasinsky93@gmail.com>
2020-11-24 11:16:34 +0300

    first site upload

---
+++ js/main.js
@@ -0,0 +1,54 @@
+/* Header fixer */
+
+let currentScroll;
+let containsFixed;
+let header = document.getElementById('header');
+let headerHeight = header.offsetHeight;
+
+let toggleFixedHeader = () => {
+    currentScroll = pageYOffset;
+    containsFixed = header.classList.contains('fixed');
+    if (currentScroll >= headerHeight && !containsFixed) {
+        header.classList.add('fixed');
+    } else if(currentScroll < headerHeight && containsFixed){
+        header.classList.remove('fixed');
+    }
+}
+
+window.addEventListener("scroll", toggleFixedHeader);
+
+
+/* Animated scroll */
+
+const navLinks = document.querySelectorAll('.nav a');
+
+for (let link of navLinks) {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const id = link.getAttribute('data-href');
+        document.querySelector(id).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'  
+        });
+        nav.classList.toggle('show-menu');
+    });
+};
+
+/* Show mobile menu */
+
+let burger = document.querySelector('#burger');
+let nav = document.querySelector('#nav');
+burger.addEventListener('click', () => {
+    nav.classList.toggle('show-menu')
+})
+
+
+/* Slick slider */
+
+$(document).ready(function(){
+    $('#reviews__slider').slick({
+        autoplay: true,
+        autoplaySpeed: 5000,
+        arrows: true
+    });
+  });
